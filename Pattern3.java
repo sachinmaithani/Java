5af9380a7ba3ea350609e9c81394086a67d62dd6
sachinmaithani <sachinmaithani88@gmail.com>
2022-07-30 00:30:27 +0530

    Create Pattern3.java

---
+++ Pattern3.java
@@ -0,0 +1,19 @@
+public class Pattern3 {
+
+	public static void main(String[] args) {
+		int n=10;
+		
+		for(int i=0;i<n;i++) {
+			for(int j=0;j<n;j++) {
+				if(  (i-j) >(n-1)/2 ||  i+j > (n-1 + (n-1)/2) ) {
+					System.out.print("*");
+				}else {
+					System.out.print(" ");
+				}
+			}
+			System.out.println();
+		}
+
+	}
+
+}
